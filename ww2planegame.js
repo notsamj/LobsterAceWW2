2371795eb787aab5952fcfa3d5e6b2121ae23e04
notsamj <samyoul.jones@gmail.com>
2023-11-24 09:31:32 -0500

    Initial Commit

---
+++ ww2planegame.js
@@ -0,0 +1,14 @@
+// Global Constants
+const CANVAS_WIDTH = 1600;
+const CANVAS_HEIGHT = 800;
+const FRAME_RATE = 240;
+// Functions
+
+function setup() {
+    createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT); // TODO: Wrong order of parameters?
+    frameRate(FRAME_RATE);
+}
+
+function draw() {
+    clear();
+}
